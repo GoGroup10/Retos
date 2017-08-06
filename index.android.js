f1f39f2c4a9ae504e57aed19df5d80572080ce77
dreicomp <dreicomp@users.noreply.github.com>
2017-08-06 13:48:52 -0500

    Primer Comid
    Se modifica Solo el index

--- index.android.js
+++ index.android.js
@@ -17,17 +17,19 @@ export default class RetosI extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
+        
+        <View style = {styles.Content2}>
+          <Text style={styles.andrei}>hola amigos, primer comid</Text>
+        </View>
+        <Text style={styles.instructions}>          
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
       </View>
-    );
+    );  
   }
 }
 
@@ -48,6 +50,15 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  Content2:{
+    flexDirection :'row',
+  },
+  andrei:{
+    flex:1,
+    textAlign:'right',
+    fontSize:22,
+    color:'#a03935',
+  }
 });
 
 AppRegistry.registerComponent('RetosI', () => RetosI);
