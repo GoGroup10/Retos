7c7b4a9def13c88bda5dfd94b41e38055e7321c4
josuesf <113552@unsaac.edu.pe>
2017-08-21 23:23:26 -0500

    Modificacion Login

--- src/LoginView.js
+++ src/LoginView.js
@@ -11,76 +11,126 @@ import {
   Text,
   Button,
   Image,
+  AsyncStorage,
 } from 'react-native';
 
-import FBSDK,{
+import FBSDK, {
   LoginButton,
   AccessToken
 } from 'react-native-fbsdk'
-import {Actions} from 'react-native-router-flux';
-import firebase,{ 
-  firebaseAuth 
+import { Actions } from 'react-native-router-flux';
+import firebase, {
+  firebaseAuth
 } from "./firebase";
 
-const {FacebookAuthProvider} = firebase.auth
+const { FacebookAuthProvider } = firebase.auth
 
 
 export default class LoginView extends Component {
   constructor() {
-     super();
+    super();
 
-     console.ignoredYellowBox = [
-         'Setting a timer'
-     ];
+    console.ignoredYellowBox = [
+      'Setting a timer'
+    ];
   }
   state = {
-    credentials:null
-  }
-  
-  componentWillMount(){
-    this.authenticateUser()
+    credencialsUser:null,
   }
 
-  authenticateUser =() =>{
-    AccessToken.getCurrentAccessToken().then((data)=>{
-      const {accessToken} = data
-      const credential = FacebookAuthProvider.credential(accessToken)
-      // Sign in user with another account
-      firebaseAuth.signInWithCredential(credential).then((credentials) => {
-        this.setState({credentials})
+  componentWillMount() {
+    //this.recuperarClaves()
+    Actions.pop()
+  }
+  guardarClaves=async(credencial)=>{
+    try {
+      await AsyncStorage.setItem('CREDENTIALS', JSON.stringify(credencial));
+    } catch (error) {
+      // Error saving data
+    }
+  }
+  recuperarClaves=async()=>{
+    try {
+      const value = await AsyncStorage.getItem('CREDENTIALS');
+      if (value !== null){
+        // We have data!!
+        const credentials=(JSON.parse(value)).credentialsUser
+        this.setState({
+          credentialsUser:{
+            uid: credentials.uid,
+            displayName: credentials.displayName,
+            email: credentials.email,
+            photoURL: credentials.photoURL,
+          }
+        })
         Actions.home()
-      }, function(error) {
-        console.log("Sign In Error", error);
-      });
+        Actions.pop({key:'login'})
+      }else{
+        this.authenticateUser()
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log(error)
+    }
+  }
+  authenticateUser = () => {
+    AccessToken.getCurrentAccessToken().then((data) => {
+      if (data) {
+        const { accessToken } = data
+        const credential = FacebookAuthProvider.credential(accessToken)
+        // Sign in user with another account
+        firebaseAuth.signInWithCredential(credential).then((credentials)  => {
+          this.setState({
+            credentialsUser:{
+              uid: credentials.uid,
+              displayName: credentials.displayName,
+              email: credentials.email,
+              photoURL: credentials.photoURL,
+            }
+          })
+          this.guardarClaves(this.state)
+          Actions.home()
+        }, function (error) {
+          console.log("Sign In Error", error);
+        });
+      }
     })
   }
-  
 
-  handleLoginFinish =(error, result) => {
-              if (error) {
-                console.error("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then((data)=>{
-                  this.authenticateUser(data.accessToken)
-                })
-              }
-            }
+
+  handleLoginFinish = (error, result) => {
+    if (error) {
+      console.error("login has error: " + result.error);
+    } else if (result.isCancelled) {
+      alert("login is cancelled.");
+    } else {
+      AccessToken.getCurrentAccessToken().then((data) => {
+        this.authenticateUser(data.accessToken)
+      })
+    }
+  }
+  hanleLogOut =async()=>{
+    try {
+      await AsyncStorage.removeItem('CREDENTIALS');
+    } catch (error) {
+      // Error saving data
+      alert('Intente de nuevo.')
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.logoContainer}>
-          <Image style={styles.logo} 
-            source={require('./imgs/logo_challenge.jpg')}/>
+          <Image style={styles.logo}
+            source={require('./imgs/logo_challenge.jpg')} />
         </View>
         <Text style={styles.welcome}>Challenge</Text>
-        
+
         <LoginButton
-          readPermissions={["public_profile","email"]}
+          readPermissions={["public_profile", "email"]}
           onLoginFinished={this.handleLoginFinish}
-          onLogoutFinished={() => alert("logout.")}/>
-      </View>  
+          onLogoutFinished={() => this.hanleLogOut()} />
+      </View>
     );
   }
 }
@@ -89,23 +139,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#353432',
-    justifyContent :'center',
-    alignItems:'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  welcome:{
-    fontSize:24,
-    fontWeight:'600',
-    marginBottom:50,
-    justifyContent :'center',
-    backgroundColor:'transparent',
-    color:'#EC268F'       
+  welcome: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 50,
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    color: '#EC268F'
   },
-  logoContainer:{
+  logoContainer: {
 
   },
-  logo:{
-    width:100,
-    height:100,
+  logo: {
+    width: 100,
+    height: 100,
   }
-  
+
 });
